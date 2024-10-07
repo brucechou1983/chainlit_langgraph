b1f3caad8dae18d8affac4e366cf4c970a74f1db
Bruce Chou <brucechou1983@gmail.com>
2024-10-07 23:57:21 +0800

    fix: conversation history issues

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 Simple demo of integration with ChainLit and LangGraph.
 """
 import chainlit as cl
-from chat_workflow.graphs.base import ChatState, create_graph
-from langchain_core.messages import HumanMessage
+from chat_workflow.graphs.base import create_graph, create_default_chat_state, ChatState
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.runnables import Runnable
 
 
@@ -13,7 +13,7 @@ async def on_chat_start():
     graph = create_graph()
 
     # initialize state
-    state = ChatState(messages=[])
+    state = create_default_chat_state()
 
     # save graph and state to the user session
     cl.user_session.set("graph", graph.compile())
@@ -24,7 +24,7 @@ async def on_chat_start():
 async def on_message(message: cl.Message):
     # Retrieve the graph and state from the user session
     graph: Runnable = cl.user_session.get("graph")
-    state = cl.user_session.get("state")
+    state: ChatState = cl.user_session.get("state")
 
     # Append the new message to the state
     state["messages"] += [HumanMessage(content=message.content)]
@@ -32,9 +32,15 @@ async def on_message(message: cl.Message):
     # Stream the response to the UI
     ui_message = cl.Message(content="")
     await ui_message.send()
+    total_content: str = ""
     async for event in graph.astream_events(state, version="v1"):
         print(f"event: {event}")
-        if event["event"] == "on_chat_model_stream" and event["name"] == "chat_llama3.2":
+        if event["event"] == "on_chat_model_stream" and event["name"] == "chat_model":
             content = event["data"]["chunk"].content or ""
+            total_content += content
             await ui_message.stream_token(token=content)
     await ui_message.update()
+
+    # Update State
+    state["messages"] += [AIMessage(content=total_content)]
+    cl.user_session.set("state", state)
--- chat_workflow/graphs/base.py
+++ chat_workflow/graphs/base.py
@@ -1,15 +1,28 @@
 import operator
+import os
+from dotenv import load_dotenv
 from typing import TypedDict, Annotated, Sequence
-from langchain_core.messages import AnyMessage, SystemMessage
+from langchain_core.messages import AnyMessage, SystemMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import Runnable, RunnableConfig
-from langchain_ollama.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
 from langgraph.graph import StateGraph, END
 from ..llm import create_chat_model
 
 
 class ChatState(TypedDict):
+    # Message history
     messages: Annotated[Sequence[AnyMessage], operator.add]
+
+    # Model name of the chatbot
+    chat_model: str
+
+
+def create_default_chat_state():
+    return {
+        "messages": [],
+        "chat_model": os.getenv("CHAT_MODEL", "ollama-llama3.2"),
+    }
 
 
 async def chat_node(state: ChatState, config: RunnableConfig) -> ChatState:
@@ -18,11 +31,11 @@ async def chat_node(state: ChatState, config: RunnableConfig) -> ChatState:
             content="You're a careful thinker. Think step by step before answering."),
         MessagesPlaceholder(variable_name="messages"),
     ])
-    llm = create_chat_model("chat_llama3.2", model="ollama-llama3.2")
-    chain: Runnable = prompt | llm
+    llm = create_chat_model("chat_model", model=state["chat_model"])
+    chain: Runnable = prompt | llm | StrOutputParser()
     response = await chain.ainvoke(state, config=config)
     return {
-        "messages": [response]
+        "messages": [AIMessage(content=response)]
     }
 
 

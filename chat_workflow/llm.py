4ff0cdd135c18f9d91ca6a8562c2182cbe02c127
Bruce Chou <brucechou1983@gmail.com>
2024-10-15 20:06:51 +0800

    fix: llama tool calling issue

--- chat_workflow/llm.py
+++ chat_workflow/llm.py
@@ -8,12 +8,11 @@ from langchain_openai import ChatOpenAI
 
 def _create_chat_ollama_model(name: str, model: str, **kwargs) -> BaseChatModel:
     stop = None
-    if model in ["llama3, llama3.1, llama3.2"]:
+    if model in ["llama3, llama3.1, llama3.2", "llama3.2:3b-instruct-q8_0"]:
         stop = [
             "<|start_header_id|>",
             "<|end_header_id|>",
-            "<|eot_id|>",
-            "<|reserved_special_token"
+            "<|eot_id|>"
         ]
 
     return ChatOllama(name=name, model=model, stop=stop, **kwargs)
@@ -32,7 +31,7 @@ def create_chat_model(name: str, model: str, tools: Optional[List] = None, **kwa
     elif match := re.match(r"^ollama-(.*)", model):
         "ex: 'ollama-llama3.2' -> 'llama3.2'"
         model_name = match.group(1)
-        return _create_chat_ollama_model(name, model_name, **kwargs)
+        llm = _create_chat_ollama_model(name, model_name, **kwargs)
     else:
         raise ValueError(f"Unsupported chat model: {model}")
 

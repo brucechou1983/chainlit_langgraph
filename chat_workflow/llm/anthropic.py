3397fafd8d8113cf5ad4312fd6d50bec63aeed7a
Bruce Chou <brucechou1983@gmail.com>
2024-10-24 10:50:18 +0800

    refactor: llm module

---
+++ chat_workflow/llm/anthropic.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from typing import List
+
+load_dotenv()
+
+
+def list_anthropic_models() -> List[str]:
+    """
+    List all available Anthropic models
+
+    Currently, the Anthropic Python module does not offer a direct API 
+    endpoint to list all available Claude models.
+    """
+    if os.getenv("ANTHROPIC_API_KEY"):
+        return ["claude-3-haiku-20240307", "claude-3-5-sonnet-20241022"]
+    else:
+        return []
--- chat_workflow/llm/ollama.py
+++ chat_workflow/llm/ollama.py
@@ -1,18 +1,12 @@
-from typing import Dict, Union, List
 import os
-import re
 import requests
 from chainlit import logger
 from dotenv import load_dotenv
-from openai import OpenAI
-from typing import Optional, List
+from typing import Dict, Union, List
 from langchain_core.language_models.chat_models import BaseChatModel
-from langchain_anthropic import ChatAnthropic
 from langchain_ollama import ChatOllama
-from langchain_openai import ChatOpenAI
 
 load_dotenv()
-
 
 # Ollama parameter type definitions
 OLLAMA_INT_PARAMS = {
@@ -89,7 +83,7 @@ def _parse_ollama_params(parameters: str) -> Dict[str, Union[int, float, List[st
     return result
 
 
-def _create_chat_ollama_model(name: str, model: str, **kwargs) -> BaseChatModel:
+def create_chat_ollama_model(name: str, model: str, **kwargs) -> BaseChatModel:
     # Fetch the model parameters from the Ollama API
     base_url = os.getenv("OLLAMA_URL")
     response = requests.post(
@@ -107,23 +101,6 @@ def _create_chat_ollama_model(name: str, model: str, **kwargs) -> BaseChatModel:
     logger.debug(f"Params kwargs: {params_kwargs}")
     return ChatOllama(name=name, model=model,
                       base_url=os.getenv("OLLAMA_URL", "http://localhost:11434"), **params_kwargs)
-
-
-def create_chat_model(name: str, model: str, tools: Optional[List] = None, **kwargs) -> BaseChatModel:
-    if re.match(r"^claude-*", model):
-        llm = ChatAnthropic(name=name, model=model, **kwargs)
-    elif re.match(r"^gpt-*", model):
-        llm = ChatOpenAI(name=name, model=model, **kwargs)
-    elif match := re.match(r"^\(ollama\)(.*)", model):
-        "ex: '(ollama)llama3.2' -> 'llama3.2'"
-        model_name = match.group(1)
-        return _create_chat_ollama_model(name, model_name, **kwargs)
-
-    if tools is None:
-        return llm
-    else:
-        return llm.bind_tools(tools)
-
 
 # List all available Ollama models by hitting the api endpoint
 # api endpoint: curl http://localhost:11434/api/tags
@@ -159,6 +136,8 @@ def create_chat_model(name: str, model: str, tools: Optional[List] = None, **kwa
 #   ]
 # }
 # when no model is available or an exception, just return an empty list
+
+
 def list_ollama_models(url: str = "http://localhost:11434") -> List[str]:
     """
     List all available Ollama models by hitting the api endpoint
@@ -172,64 +151,3 @@ def list_ollama_models(url: str = "http://localhost:11434") -> List[str]:
         return [f'(ollama){model["name"]}' for model in response.json()["models"]]
     except:
         return []
-
-# List all openai models
-
-
-def list_openai_models() -> List[str]:
-    """
-    List all available OpenAI models
-
-    Sample Response:
-    {
-      "object": "list",
-      "data": [
-        {
-          "id": "model-id-0",
-          "object": "model",
-          "created": 1686935002,
-          "owned_by": "organization-owner"
-        },
-        {
-          "id": "model-id-1",
-          "object": "model",
-          "created": 1686935002,
-          "owned_by": "organization-owner",
-        },
-        {
-          "id": "model-id-2",
-          "object": "model",
-          "created": 1686935002,
-          "owned_by": "openai"
-        },
-      ],
-      "object": "list"
-    }
-    """
-    try:
-        client = OpenAI()
-        response = client.models.list()
-        # Only use model id starting with "gpt-4o-"
-        return [f'{model.id}' for model in response.data if model.id.startswith("gpt-4o-")]
-    except Exception as e:
-        return []
-
-
-def list_anthropic_models() -> List[str]:
-    """
-    List all available Anthropic models
-
-    Currently, the Anthropic Python module does not offer a direct API 
-    endpoint to list all available Claude models.
-    """
-    if os.getenv("ANTHROPIC_API_KEY"):
-        return ["claude-3-haiku-20240307", "claude-3-5-sonnet-20241022"]
-    else:
-        return []
-
-
-def list_available_llm() -> List[str]:
-    """
-    List all available models
-    """
-    return list_ollama_models(os.getenv("OLLAMA_URL", "http://localhost:11434")) + list_openai_models() + list_anthropic_models()
